5e248420a02c93a649dcb7aff2bdb24103c0fdc1
Astrum <kts6481@gmail.com>
2023-05-02 10:14:35 +0900

    add kiosk_fnc

---
+++ Python_Basic_ch09_kiosk/kiosk_fnc.py
@@ -0,0 +1,13 @@
+#kiosk에 필요한 기능들(함수)
+#1. 세부 메뉴 출력
+def print_sub_menu():
+    pass
+# 2. 사용자 메뉴 선택
+def choice_menu_num():
+    while True:
+        menu_num = int(input(">>번호 : "))
+        if menu_num >= 1 and menu_num <= 3:
+            # if menu_num=="1" or "2" or "3" or "4":
+            break
+        else:
+
